c4ad2572237cc7cf3042414551803f35b2e3b5a2
cjj <15102239723@163.com>
2025-09-15 17:13:02 +0800

    看提交之后绿色竖条还在不在

--- 求阶乘.py
+++ 求阶乘.py
@@ -1,8 +1,8 @@
-'''求出某个自然数的阶乘。一个正整数的阶乘是所有小于等于该数的正整数的积
-并且0的阶乘为1，自然数n的阶乘写作n!'''
+"""求出某个自然数的阶乘。一个正整数的阶乘是所有小于等于该数的正整数的积
+并且0的阶乘为1，自然数n的阶乘写作n!"""
 
 #方法一：非递归
-sum=1
+'''sum=1
 n=eval(input("请输入一个自然数："))
 while n<0:
     print("输入有误，请重新输入：")
@@ -10,7 +10,7 @@ while n<0:
 else:
     for i in range(1,n+1):
         sum*=i
-    print(sum)
+    print(sum)'''
 
 #方法二：递归
 def jie_cheng(num):
@@ -18,3 +18,10 @@ def jie_cheng(num):
         return 1
     else:
         return num*jie_cheng(num-1)
+def main():
+    m = eval(input("请输入一个自然数："))
+    while m<0:
+        print("输入有误，请重新输入：")
+        m = eval(input("请输入一个自然数："))
+    else:
+        print(jie_cheng(m))
